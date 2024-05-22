5df1b62e496d0b5f2281f2a537ab8ce395348b44
israel <imru@fixstars.com>
2024-05-22 12:15:57 -0700

    added in config

---
+++ log-viewer/backend/src/main/java/net/griddb/gridlog/logviewer/ConfigInfo.java
@@ -0,0 +1,16 @@
+package net.griddb.gridlog.logviewer;
+
+import com.toshiba.mwcloud.gs.*;
+
+class ConfigInfo {
+    @RowKey String logtype;
+    String file_path;
+    Integer interval;
+    Integer expiration_time;
+    Integer partition_unit;
+    Integer timestamp_position;
+    String regex_format;
+    String timestamp_format;
+    String[] schemaArr;
+   // HashMap<String,String> schema;
+}
