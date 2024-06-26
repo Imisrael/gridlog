5b0dbf27ee49dbf030c2f02430ce65aedbe6b36d
israel <imru@fixstars.com>
2024-06-26 11:55:45 -0700

    got jdbc sql achemy working with pandas read_sql command

--- machine-learning/griddb.py
+++ machine-learning/griddb.py
@@ -1,10 +1,30 @@
 import jpype
 import jpype.dbapi2
+from sqlalchemy import create_engine
+from sqlalchemy.engine.url import URL
+import pandas as pd
 
 jpype.startJVM(jpype.getDefaultJVMPath(), "-ea", "-Djava.class.path=/app/lib/gridstore-jdbc-5.6.0.jar")
 
-url = "jdbc:gs://griddb-server:20001/myCluster/public"
-conn = jpype.dbapi2.connect(url, driver="com.toshiba.mwcloud.gs.sql.Driver", driver_args={"user": "admin", "password": "admin"})
-curs = conn.cursor()
-curs.execute("SELECT timestamp  FROM LOG_agent_intrusion_exploit")
-rows = curs.fetchall()
+eng_url = URL.create(
+    drivername='sqlajdbc',
+    host='/myCluster/public',
+    query={
+        '_class': 'com.toshiba.mwcloud.gs.sql.Driver',
+        '_driver': 'gs',
+        'user': 'admin',
+        'password': 'admin',
+        'notificationMember': 'griddb-server:20001',
+        '_jars':  '/app/lib/gridstore-jdbc-5.6.0.jar'
+    }
+)
+
+eng = create_engine(eng_url)
+
+with eng.connect() as c:
+    print("Connected")
+    # res = c.execute('SELECT CURRENT_DATE').fetchall()
+    # print(res)
+    df = pd.read_sql("SELECT timestamp FROM LOG_agent_intrusion_exploit", c)
+    print(df.head())
+
